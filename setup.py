400b0fec57b5a847275174654f3b880f8ff57d1e
William Grzybowski <wg@FreeBSD.org>
2013-06-22 18:23:35 -0300

    Commit initial scope for the project

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+from distutils.core import setup, Extension
+
+from sysctl import get_version
+
+modules = [
+    Extension(
+        'sysctl/sysctl',
+        sources=['sysctl/sysctl.c'],
+    )
+]
+
+setup(
+    name='sysctl',
+    version=get_version(),
+    url='https://github.com/williambr/py-sysctl',
+    license='BSD',
+    author='William Grzybowski',
+    author_email='wg@FreeBSD.org',
+    description='Wrapper for the sysctl* system functions',
+    long_description=open(
+        os.path.join(
+            os.path.dirname(__file__),
+            'README'
+        )
+    ).read(),
+    keywords='sysctl',
+    packages=('sysctl', ),
+    platforms='any',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License'
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Programming Language :: Python',
+    ],
+    cmdclass={'build_py': build_py},
+    ext_modules=modules,
+)
